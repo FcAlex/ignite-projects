d7c4548277848a0ce479c3e3122bc183d1e8af7f
FcAlex <contato.fcoalex@gmail.com>
2022-04-10 18:09:31 -0300

    adiciona possibilidade de definir qual o usuário ou organização buscar

--- reactjs/github-explorer/src/components/RepositoryList.tsx
+++ reactjs/github-explorer/src/components/RepositoryList.tsx
@@ -12,12 +12,14 @@ type Repository = {
 
 export function RepositoryList() {
   const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [username, setUsername] = useState("");
+  const [isOrg, setIsOrg] = useState(false);
 
-  useEffect(() => {
-    fetch('https://api.github.com/orgs/rocketseat/repos')
-      .then(response => response.json())
-      .then(data => setRepositories(data))
-  }, []); // componentDidMount
+  // useEffect(() => {
+  //   fetch('https://api.github.com/orgs/rocketseat/repos')
+  //     .then(response => response.json())
+  //     .then(data => setRepositories(data))
+  // }, []);
 
   function createRepositoryItems(repository: Repository) {
     const repositoryProps = {
@@ -29,9 +31,37 @@ export function RepositoryList() {
     return <RepositoryItem key={repository.node_id} repository={repositoryProps} />
   }
 
+  function getRepositories() {
+    fetch(`https://api.github.com/${isOrg ? 'orgs' : 'users'}/${username}/repos`)
+      .then(response => {
+        if(!response.ok) 
+          throw new Error();
+        
+        return response.json()
+      })
+      .then(data => setRepositories(data))
+      .catch(_ => setRepositories([]))
+  }
+
   return (
-    <section className="repository-list">
+    <section className="repository-list">      
       <h1>Lista de Repositórios</h1>
+
+      <form onSubmit={e => e.preventDefault()}>
+        <div className="is-org">
+          <input 
+            type="checkbox" 
+            id="isOrg"
+            checked={isOrg}
+            onChange={e => setIsOrg(e.target.checked)}/> 
+          <label htmlFor="isOrg"> É uma organização?</label>
+        </div>  
+
+        <div className="search">
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+          <button type="button" onClick={getRepositories}>Buscar</button>
+        </div>
+      </form>
 
       <ul>
         {repositories && repositories.map(createRepositoryItems)}
