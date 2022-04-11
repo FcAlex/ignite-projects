4400a7f678c031647bc1352d647f459999cf6cab
FcAlex <contato.fcoalex@gmail.com>
2022-04-10 21:14:55 -0300

    adiciona implementação de watchme

---
+++ reactjs/desafio2/src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+import { Button } from './components/Button';
+import { MovieCard } from './components/MovieCard';
+
+// import { SideBar } from './components/SideBar';
+// import { Content } from './components/Content';
+
+import { api } from './services/api';
+
+import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  function handleSelectedGenreId(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar setIdUpdated={handleSelectedGenreId}/>
+      <Content selectedGenreId={selectedGenreId} />
+    </div>
+  )
+}
