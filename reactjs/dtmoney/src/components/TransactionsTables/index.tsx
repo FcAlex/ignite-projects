4d3851afc8eb74fc5c63002e516cc145a7426702
FcAlex <contato.fcoalex@gmail.com>
2022-04-11 23:24:16 -0300

    adiciona criação de modal, fakeapi e criação de transação

--- reactjs/dtmoney/src/components/TransactionsTables/index.tsx
+++ reactjs/dtmoney/src/components/TransactionsTables/index.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string
+}
+
 export function TransactionsTables() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  useEffect(() => {
+    api.get('transations')
+      .then(response => setTransactions(response.data.transactions))
+  }, [])
+
   return (
     <Container>
       <table>
@@ -14,26 +32,23 @@ export function TransactionsTables() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$ 12.000</td>
-            <td>Trabalho</td>
-            <td>20/02/2021</td>
-          </tr>
-
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">-R$ 400</td>
-            <td>Casa</td>
-            <td>25/02/2021</td>
-          </tr>
-
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td>R$12.000</td>
-            <td>Trabalho</td>
-            <td>20/02/2021</td>
-          </tr>
+          {transactions && transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)} 
+              </td>
+              <td>{transaction.type}</td>
+              <td>
+              {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
