7002b70681af4f4cf37b91815bac260b276bcc04
FcAlex <contato.fcoalex@gmail.com>
2022-04-21 00:08:49 -0300

    Adiciona layout

---
+++ reactjs/ignews/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image alt='ig.news' src='/images/logo.svg' width='100%' height='100%'/>
+
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
