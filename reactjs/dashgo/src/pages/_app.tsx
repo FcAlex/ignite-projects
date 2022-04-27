cb7cc665e9e856cb88eb48a9f2a35589222abf92
FcAlex <contato.fcoalex@gmail.com>
2022-04-26 22:28:00 -0300

    adiciona page de signin e cria componente de header

--- reactjs/dashgo/src/pages/_app.tsx
+++ reactjs/dashgo/src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { Theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={Theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
